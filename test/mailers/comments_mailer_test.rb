0e0a01ffdf0bc3fab8add3d45396c4a20e1d4ad9
Ventura <90869975+ventu-ra@users.noreply.github.com>
2023-04-30 02:12:00 -0300

    Update comments_mailer_test.rb

--- test/mailers/comments_mailer_test.rb
+++ test/mailers/comments_mailer_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class CommentsMailerTest < ActionMailer::TestCase
   test "submitted" do
     mail = CommentsMailer.submitted comments(:one)
-    assert_equal "New comment!", mail.subject
+    assert_equal "Ne comment!", mail.subject
     assert_equal ["rabete8023@larland.com"], mail.to
     assert_equal ["from@example.com"], mail.from
   end
