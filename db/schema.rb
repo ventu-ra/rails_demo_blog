a923a2f80ea7a02f96a061a50ba98b4f11c50035
ventu-ra <ventura09.10.0@gmail.com>
2024-08-07 19:57:41 -0400

    config to deploy on render

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_04_30_004058) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -50,7 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_30_004058) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.integer "post_id", null: false
+    t.bigint "post_id", null: false
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
