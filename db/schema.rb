c78345baef880b0c8960f62c9646155fddbf6d92
ventu-ra <ventura09.10.0@gmail.com>
2025-08-24 11:14:35 -0300

    chore(database): switch from PostgreSQL to SQLite
    - Updated Gemfile to use sqlite3 instead of pg.
    - Modified database.yml to configure SQLite settings.
    - Adjusted schema.rb to remove PostgreSQL extension.
    - Updated data types in comments table for compatibility.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_04_30_004058) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -53,7 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_30_004058) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.bigint "post_id", null: false
+    t.integer "post_id", null: false
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
